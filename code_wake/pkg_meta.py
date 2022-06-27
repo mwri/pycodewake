761472cdf31fd638355936b432040c8ee329fda8
Michael Wright <mjw@methodanalysis.com>
2022-06-27 20:14:24 +0100

    release 0.0.4

--- code_wake/pkg_meta.py
+++ code_wake/pkg_meta.py
@@ -4,7 +4,7 @@ import sys
 from typing import Any, Dict
 
 name = "code-wake"
-version = "0.0.3"
+version = "0.0.4"
 author = "Michael Wright"
 author_email = "mjw@methodanalysis.com"
 description = "Running code use instrumentation aid"
